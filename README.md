380f0157b38d9521f05cf1da346349905fef65db
devincarr <devin.carr0@gmail.com>
2015-12-15 00:22:55 -0600

    Added files.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # remindme
 A simple script to open a video after a time delay to remind me of a small task.
+
+# Building
+
+`gcc remindme.c -o remindme`
+
+# Help
+
+```
+remindme help:
+A simple script to open a http link after a designated time limit.
+
+-t <minutes>	sets the time limit to wait in minutes(can be a float value).
+-l <http-link>	sets the link to open after the sleep.
+```
---
+++ remindme.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+	/* wait time is in minutes */
+	double wait_time = 1.0;
+	char* link = "https://www.youtube.com/watch?v=6GggY4TEYbk";
+	for (int i = 1; i < argc; i += 2) {
+		if (strncmp(argv[i], "-t", 2) == 0)
+			wait_time = atof(argv[i+1]);
+		else if (strncmp(argv[i], "-l", 2) == 0)
+			link = argv[i+1];
+		else if (strncmp(argv[i], "help", 4) == 0 || strncmp(argv[i], "-h",2) == 0) {
+			printf("remindme help:\nA simple script to open a http link after a designated time limit.\n\n-t <minutes>\tsets the time limit to wait in minutes(can be a float value).\n-l <http-link>\tsets the link to open after the sleep.\n");
+			return 0;
+		}
+	}
+
+	/* Setup before the sleep */
+	char buf[256];
+	char* open = "open ";
+        strcpy(buf,open);
+        strcat(buf,link);
+
+	printf("I will remind you in %f minutes.\n", wait_time);
+	printf("Link: %s\n",buf);
+
+	/* Sleep the desired time (note: sleep() takes seconds) */
+	sleep(wait_time*60.0);
+	system(buf);
+	return 1;
+}
